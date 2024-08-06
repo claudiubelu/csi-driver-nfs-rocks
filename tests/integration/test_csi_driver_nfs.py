19e63c375c5906a3fbfb980ca875caea8f4653d7
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-08-06 06:51:00 +0000

    Adds probe checks

--- tests/integration/test_csi_driver_nfs.py
+++ tests/integration/test_csi_driver_nfs.py
@@ -5,15 +5,9 @@
 import logging
 
 from k8s_test_harness import harness
-from k8s_test_harness.util import env_util, k8s_util
+from k8s_test_harness.util import env_util, exec_util, k8s_util
 
 LOG = logging.getLogger(__name__)
-
-CSI_PROVISIONER_IMG = "ghcr.io/canonical/csi-provisioner:4.0.0-ck0"
-LIVENESSPROBE_IMG = "ghcr.io/canonical/livenessprobe:2.12.0-ck0"
-NODE_DRIVER_REGISTRAR_IMG = "ghcr.io/canonical/csi-node-driver-registrar:2.10.0-ck0"
-SNAPSHOT_CONTROLLER_IMG = "ghcr.io/canonical/snapshot-controller:6.3.3-ck0"
-SNAPSHOTTER_IMG = "ghcr.io/canonical/csi-snapshotter:6.3.3-ck0"
 
 
 def _get_nfsplugin_csi_helm_cmd(version: str):
@@ -21,11 +15,6 @@ def _get_nfsplugin_csi_helm_cmd(version: str):
 
     images = [
         k8s_util.HelmImage(rock.image, subitem="nfs"),
-        k8s_util.HelmImage(CSI_PROVISIONER_IMG, subitem="csiProvisioner"),
-        k8s_util.HelmImage(LIVENESSPROBE_IMG, subitem="livenessProbe"),
-        k8s_util.HelmImage(NODE_DRIVER_REGISTRAR_IMG, subitem="nodeDriverRegistrar"),
-        k8s_util.HelmImage(SNAPSHOT_CONTROLLER_IMG, subitem="externalSnapshotter"),
-        k8s_util.HelmImage(SNAPSHOTTER_IMG, subitem="csiSnapshotter"),
     ]
 
     set_configs = [
@@ -47,4 +36,13 @@ def test_nfsplugin_integration(function_instance: harness.Instance):
 
     k8s_util.wait_for_daemonset(function_instance, "csi-nfs-node", "kube-system")
     k8s_util.wait_for_deployment(function_instance, "csi-nfs-controller", "kube-system")
-    k8s_util.wait_for_deployment(function_instance, "snapshot-controller", "kube-system")
+    k8s_util.wait_for_deployment(
+        function_instance, "snapshot-controller", "kube-system"
+    )
+
+    # call the nfsplugin's liveness probes to check that they're running as intended.
+    for port in [29652, 29653]:
+        # It has hostNetwork=true, which means that curling localhost should work.
+        exec_util.stubbornly(retries=5, delay_s=5).on(function_instance).exec(
+            ["curl", f"http://localhost:{port}/healthz"]
+        )
